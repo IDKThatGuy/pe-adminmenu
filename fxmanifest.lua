7387a334e742efabe0ec10cbc8ca36b42ec748c0
Fezz <43589668+FBFezz@users.noreply.github.com>
2021-02-24 14:23:18 +0000

    Fixed locales not showing up

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -8,21 +8,19 @@ description 'An admin menu created with esx_menu_default by Project-Entity'
 
 version '2.0.1'
 
-shared_script {
-    'config.lua'
-}
+shared_script 'config.lua'
 
 client_scripts {
     '@es_extended/locale.lua',
+     'locales/es.lua',
+    'locales/en.lua',
     'client/admin_cl.lua',
-    'client/entityiter.lua',
-    'locales/es.lua',
-    'locales/en.lua'
+    'client/entityiter.lua'
 }
 
 server_scripts {
     '@es_extended/locale.lua',
-    'server/admin_sv.lua',
     'locales/es.lua',
-    'locales/en.lua'
+    'locales/en.lua',
+    'server/admin_sv.lua'
 }
